62af81064f4f9519036516d20cd8553bffaf69fe
Sang Won Han <petershan0315@gmail.com>
2018-03-29 23:06:55 +0900

    collectstatic을 배포후 실행코드에 추가

--- .config/deploy.sh
+++ .config/deploy.sh
@@ -3,6 +3,10 @@ sudo rm -rf /etc/nginx/sites-available/*
 sudo cp -f /srv/ec2-deploy/.config/nginx-app.conf /etc/nginx/sites-available/nginx-app.conf
 sudo ln -sf /etc/nginx/sites-available/nginx-app.conf /etc/nginx/sites-enabled/nginx-app.conf
 sudo cp -f /srv/ec2-deploy/.config/uwsgi.service /etc/systemd/system/uwsgi.service
+cd /srv/ec2-deploy/app
+/bin/bash -c \
+'/home/ubuntu/.pyenv/versions/ip-deploy-practice-3/bin/python \
+/srv/ec2-deploy/app/manage.py collectstatic --noinput' ubuntu
 sudo systemctl enable uwsgi
 sudo systemctl daemon-reload
 sudo systemctl restart uwsgi nginx
--- app/config/settings.py
+++ app/config/settings.py
@@ -14,6 +14,8 @@ import os
 import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import raven
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
@@ -56,7 +58,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'photos',
+
+    'raven.contrib.django.raven_compat',
+    'django_extensions',
 ]
+
+RAVEN_CONFIG = {
+    'dsn': 'https://c4e5cd05ecc941418964fa2648c5c5e6:b9657eef32344edbb526526f46b1b5df@sentry.io/298196',
+    # If you are using git, you can also automatically configure the
+    # release based on the git info.
+    'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -131,3 +143,48 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR', # To capture more than ERROR, change to WARNING, INFO, etc.
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+            'tags': {'custom-tag': 'x'},
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
